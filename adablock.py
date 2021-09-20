5e7ccd1686ec4436bddf8118b4af59cd31e44845
mik1893 <michele.roscelli@gmail.com>
2021-09-20 17:59:41 +0100

    Drop sample comment from api example

--- adablock.py
+++ adablock.py
@@ -2,22 +2,6 @@
 # pylint: disable=C0116,W0613
 # This program is dedicated to the public domain under the CC0 license.
 
-"""
-Simple Bot to send timed Telegram messages.
-
-This Bot uses the Updater class to handle the bot and the JobQueue to send
-timed messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Alarm Bot example, sends a message after a set time.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import sqlite3
